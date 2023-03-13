b5d5f084c4f102f4c96d2fde1a72a6371aacddbf
Wahil-dev <wahil.chettouf@laplateforme.io>
2023-03-13 15:38:41 +0100

    head2 file, connexion file, b_conn file

--- connexion.php
+++ connexion.php
@@ -1,7 +1,14 @@
 <?php
     require_once("inc/User.php");
     $user->redirect_if_logged();
-    if($user->connect(login: "bvb", password: "bvb")) {
-        header("location: profile.php");
-        exit();
-    }
+?>
+<?php require_once("inc/head2.php")?>
+        <form class="form" action="inc/b_conn.php">
+            <input type="text" name="login" id="login" class="inp">
+            <input type="password" name="password" id="password" class="inp">
+
+            <input type="submit" value="connexion" class="btn-custom">
+        </form>
+    </div>
+</body>
+</html>
