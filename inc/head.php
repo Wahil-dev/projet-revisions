bfd74ac50ab771ad518d482b6199f046dcaad291
Wahil-dev <wahil.chettouf@laplateforme.io>
2023-03-13 15:13:45 +0100

    effacer les leins de bootstrap / header, footer , menu

---
+++ inc/head.php
@@ -0,0 +1,34 @@
+<?php
+    require_once("inc/User.php");
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>index</title>
+</head>
+<body>
+    <header class="">
+        <a href="index.php"><?= $user->is_logged() ? $user->get_login() : "visiteur"?></a>
+        <nav>
+            <?php if($user->is_logged()) : ?>
+                <ul class="menu flex-r">
+                    <li><a href="index.php">acceuil</a></li>
+                    <li><a href="articles.php">articles</a></li>
+                    <li><a href="profile.php">profile</a></li>
+                    <li><a href="inscription.php">new_article</a></li>
+                    <li><a href="inc/deconnexion.php">déconnexion</a></li>
+                </ul>
+            <?php else :?>
+                <ul class="menu flex-r">
+                    <li><a href="index.php">acceuil</a></li>
+                    <li><a href="articles.php">articles</a></li>
+                    <li><a href="connexion.php">connexion</a></li>
+                    <li><a href="inscription.php">inscription</a></li>
+                </ul>
+            <?php endif ?>
+        </nav>
+    </header>
+    <div class="container">
--- index.php
+++ index.php
@@ -1,1 +1,5 @@
-<a href="connexion.php">connexion</a>
+
+    <?php 
+        require_once("inc/head.php");
+        require_once("inc/footer.php");
+    ?>
