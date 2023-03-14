120bc96f3e25f844ecc92da24851443bcbc96b18
Wahil-dev <wahil.chettouf@laplateforme.io>
2023-03-14 12:38:18 +0100

    profile page (maintenance)

--- profile.php
+++ profile.php
@@ -1,9 +1,15 @@
 <?php
     require_once("inc/User.php");
     $user->redirect_if_not_logged();
+?>
+
+
+<?php
+    require_once("inc/head.php");
+    
     echo "<pre>";
         var_dump($user->get_user_info());
     echo "</pre>";
+    
+    require_once("inc/footer.php");
 ?>
-
-<a href="inc/deconnexion.php">déconnexion</a>
